e1171a206f0aa5b3388d1dc9628c2f9201a12b9a
Thomas Rizzo <rizzot@oregonstate.edu>
2025-10-01 22:46:51 -0700

    Added command line arguments.. can't tell if non-default port numbers work yet though

--- webclient.py
+++ webclient.py
@@ -1,22 +1,29 @@
 import socket
+import argparse
 
 
-def HTTP_Request(method="GET", url="example.com", port=80):
+def HTTP_Request(url, port):
     s = socket.socket()
     server = (url, port)
     s.connect(server)
-    request = f"{method} / HTTP/1.1\r\nHost:www.{url}\r\nConnection: close\r\n\r\n"
+    request = f"GET / HTTP/1.1\r\nHost:{url}\r\nConnection: close\r\n\r\n"
     request = request.encode("ISO-8859-1")
     s.sendall(request)
-    data = s.recv(1024)
-    response = data.decode("ISO-8859-1")
-    print(response)
+    while True:
+        data = s.recv(4096)
+        if len(data) == 0:
+            break
+        response = data.decode("ISO-8859-1")
+        print(response)
     s.close()
 
+parser = argparse.ArgumentParser(description="Specify URL and port number if you wish")
+parser.add_argument("url", nargs="?", default="www.example.com", help="Enter a valid URL")
+parser.add_argument("port", nargs="?", type = int, default=80, help="Enter a valid port number")
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    HTTP_Request()
-
+    HTTP_Request(args.url, args.port)
 
 
 # This needs to run with 'python3 webclient.py example.com'
