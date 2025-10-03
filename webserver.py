7ec47f3060faac2b9c9ab73732408f1e221c29fe
Thomas Rizzo <rizzot@oregonstate.edu>
2025-10-03 11:45:38 -0700

    Data can be now be sent and read between server and client. Still need to figure out closing connection after response

--- webserver.py
+++ webserver.py
@@ -5,20 +5,16 @@ def WebServer(port):
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('', port))
-    s.listen()
-    print("Listening for incoming connections... ")
     while True:
+        s.listen()
+        print("Listening for incoming connections... ")
         new_conn = s.accept()
         new_socket = new_conn[0]
-        print("meow")
-        while True:
-            data = new_socket.recv(4096)
-            data.decode("ISO-8859-1")
-            if data == "\r\n\r\n":
-                break
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!\r\n\r\n"
-        response.encode("ISO-8859-1")
-        new_socket.sendall(response)
+        data = new_socket.recv(1024).decode("ISO-8859-1")
+        print(data)
+        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!\r\n\r\n'
+        new_socket.sendall(response.encode("ISO-8859-1"))
+        new_socket.close()
 
 parser = argparse.ArgumentParser(description="Specify a port number if you wish")
 parser.add_argument("port", nargs="?", type=int, default=28333, help="Enter a valid port number")
